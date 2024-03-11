7524f75e0c63ad195b0aa60515ad853ecd42187b
R1ngZer0 <cbodungen@outlook.com>
2024-03-11 18:26:03 -0500

    Updated 9-2

--- Chapter 9/Recipe 9-4/private_api.py
+++ Chapter 9/Recipe 9-4/private_api.py
@@ -3,10 +3,9 @@ import requests
 url = "http://localhost:8001/v1/chat/completions" 
 
 headers = {"Content-Type": "application/json"} 
-
 data = { "messages": [
     {
-        "content": "Analyze the Incident Response Plan for key strategies"
+        "content": "Analyze the Incident Response Plan for potential gaps and weaknesses."
     }
 ], 
     "use_context": True, 
@@ -16,5 +15,5 @@ data = { "messages": [
 } 
 
 response = requests.post(url, headers=headers, json=data) 
-result = response.json() 
+result = response.json().get('choices')[0].get('message').get('content').strip()
 print(result) 
